54dd0054f9c876c65c01b5064dafa5aa9b1fb1ea
Xiaopeng Yan <xiaopeng.yan@daimler.com>
2018-12-05 16:06:52 +0800

    Add LinkedListPractice class

---
+++ src/algPractice/LinkedListPractice.java
@@ -0,0 +1,19 @@
+package algPractice;
+
+/**
+ * 1. Revert single list
+ * 2. Loop check
+ * 3. Merge two sorted list
+ * 4. Delete the nth node from the bottom of the list
+ * 5. Find the middle node of the list
+ * @author yxp
+ *
+ */
+public class LinkedListPractice {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
